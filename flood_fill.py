883d050167ce26b2cf9ea86fe869e45d5563394e
Daria290804 <mikryukova.dd20@gmail.com>
2024-04-22 12:30:57 +0200

    message

--- flood_fill.py
+++ flood_fill.py
@@ -1,17 +1,53 @@
 import matplotlib.pyplot as plt
 
 
+# flood_fill.py
+
+def flood_fill(image, x, y):
+    """Perform flood fill algorithm."""
+    rows = len(image)
+    cols = len(image[0])
+
+    # Check if pixel is out of image bounds
+    if x < 0 or x >= rows or y < 0 or y >= cols:
+        return image
+
+    # Check if pixel is background or already filled
+    if image[x][y] == 0 or image[x][y] == 2:
+        return image
+
+    # Change pixel value to filled
+    image[x][y] = 2
+
+    # Recursively call flood_fill for 4 neighboring pixels
+    flood_fill(image, x + 1, y)  # Down
+    flood_fill(image, x - 1, y)  # Up
+    flood_fill(image, x, y + 1)  # Right
+    flood_fill(image, x, y - 1)  # Left
+
+    return image
+
+
 def main():
-    img = plt.imread("files/img0.png")[:, :, 0]
-    # img = plt.imread("files/img1.png")[:, :, 0]
-    # img = plt.imread("files/img2.png")[:, :, 0]
+    # Example image (2D array)
+    image = [
+        [1, 1, 0, 0, 0],
+        [0, 1, 1, 0, 1],
+        [0, 0, 1, 0, 0],
+        [1, 0, 1, 1, 0],
+        [0, 0, 1, 0, 1]
+    ]
 
-    # img = floodfill(img, 0, 0)
+    # Starting seed coordinates (0-based index)
+    x = 0
+    y = 0
 
-    plt.imshow(img, cmap="gray")
-    plt.show(block=False)
-    plt.pause(5)
-    plt.clf()
+    # Perform flood fill
+    result = flood_fill(image, x, y)
+
+    # Print result
+    for row in result:
+        print(row)
 
 
 if __name__ == "__main__":
