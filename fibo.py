ed9ffb77640152ed9f1467181ace341af74696f2
Daria290804 <mikryukova.dd20@gmail.com>
2024-04-22 12:05:21 +0200

    message

--- fibo.py
+++ fibo.py
@@ -1,6 +1,16 @@
+def recursive_nth_fibo(n):
+    """Calculate the n-th Fibonacci number recursively."""
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return recursive_nth_fibo(n - 1) + recursive_nth_fibo(n - 2)
 
 def main():
-    pass
+    n = int(input("Enter the number of Fibonacci sequence elements: "))
+    fibo_sequence = [recursive_nth_fibo(i) for i in range(1, n + 1)]
+    print("Fibonacci sequence:", fibo_sequence)
 
 
 if __name__ == "__main__":
